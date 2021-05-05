a2ae708af594fb3a79d7125baec779225a97d762
Adriannguyen <phuducnguyen108@gmail.com>
2021-05-05 18:10:14 +0700

    final

--- GameNim/main.cpp
+++ GameNim/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <time.h>
+#include <stdlib.h>
+#include "visual.h"
 
+using namespace std;
+
+int main()
+{
+    //Sound_Game(1) ;
+    //srand(time(0)) ;
+    init();
+    menu();
+    gamePlay();
+    return 0;
+}
